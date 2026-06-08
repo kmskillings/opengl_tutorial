5cdfd0e865fbd75e711d6776cb2bd727756221f4
Kenneth Skillings <kmskillings@gmail.com>
2026-06-07 18:15:40 -0600

    Open a GLFW window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello World!\n");
+#include <glfw3.h>
+
+int main(void)
+{
+    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Tutorial", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+    while(glfwWindowShouldClose(window) == GL_FALSE)
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwSetWindowShouldClose(window, GL_TRUE);
+        }
+    }
+
+    glfwTerminate();
+
     return EXIT_SUCCESS;
 }
