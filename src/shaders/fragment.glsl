4266449ea9c1158879fe6a4a827e03d0e4179f5a
Kenneth Skillings <kmskillings@gmail.com>
2026-06-07 20:43:53 -0600

    Pass in color via attribute

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,10 +1,10 @@
 #version 150 core
 
-uniform vec3 triangleColor;
+in vec3 colorFrag;
 
 out vec4 outColor;
 
 void main()
 {
-    outColor = vec4(triangleColor, 1.0);
+    outColor = vec4(colorFrag, 1.0);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,8 +1,12 @@
 #version 150 core
 
 in vec2 position;
+in vec3 colorVert;
+
+out vec3 colorFrag;
 
 void main()
 {
     gl_Position = vec4(position, 0.0, 1.0);
+    colorFrag = colorVert;
 }
