726a6a1feeef474f842705171dafc0ea36a545b1
Kenneth Skillings <kmskillings@gmail.com>
2026-06-09 21:36:51 -0600

    Factor shaders out of main

---
+++ src/shaders/shaders.c
@@ -0,0 +1,9 @@
+#include "shaders.h"
+
+const char vertexShaderSource[] = {
+    #include "vertex.xxd"
+};
+
+const char fragmentShaderSource[] = {
+    #include "fragment.xxd"
+};
---
+++ src/shaders/shaders.h
@@ -0,0 +1,7 @@
+#ifndef _SHADERS_H
+#define _SHADERS_H
+
+extern const char vertexShaderSource[];
+extern const char fragmentShaderSource[];
+
+#endif
