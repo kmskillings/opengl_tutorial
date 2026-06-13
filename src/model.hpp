c05640fd55ed9034ee0e2205bd4f3f1a4f15cce9
Kenneth Skillings <kmskillings@gmail.com>
2026-06-13 14:55:38 -0600

    Draft Model code

--- src/model.hpp
+++ src/model.hpp
@@ -1,8 +1,32 @@
 #ifndef _MODEL_HPP
 #define _MODEL_HPP
 
-class Model {
+#include "gl_includes.h"
+#include "material.hpp"
+#include "mesh.hpp"
 
+namespace GlWorld {
+
+class Model {
+public:
+    Model(){
+        // Bind any vertex attributes used by the Material
+        this->material = new Material();
+        this->mesh = new Mesh();
+        this->material->bindVertexAttributes(this->mesh);
+    }
+    void drawRgb(void) {
+        this->material->activate();     // Call useShaderProgram on any shader programs connected to the material and send any material-stored uniforms.
+        this->mesh->activate();         // Bind any vaos of the mesh
+        this->mesh->draw();             // Make the appropriate draw calls on the mesh
+        this->mesh->deactivate();       // Un-use any shader programs
+        this->material->deactivate();   // Unbind any vaos of the mesh.
+    }
+private:
+    Material* material;
+    Mesh* mesh;
 };
+
+}
 
 #endif
