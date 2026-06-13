e24067635281d78e7f5868d035ee44867a9bd82d
Kenneth Skillings <kmskillings@gmail.com>
2026-06-13 15:38:38 -0600

    Animate triangle color

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <glew.h>
 #include <glfw3.h>
 
+#include <chrono>
+#include <cmath>
+
 #include "material.hpp"
 #include "mesh.hpp"
 #include "model.hpp"
@@ -11,10 +14,15 @@
 #define WINDOW_HEIGHT 600
 #define WINDOW_TITLE "Cami Cube"
 
+#define THROB_PERIOD_MILLIS 2000
+
 GLFWwindow* setupGl(void);
+uint64_t millisSinceEpoch(void);
 
 int main(void)
 {
+
+    constexpr float PI_F = 3.1415927f;
 
     // Setup code
     GLFWwindow* window = setupGl();
@@ -30,8 +38,18 @@ int main(void)
     GlWorld::Model* model = new GlWorld::Model(color);
     GLuint error = glGetError();
 
+    uint64_t millisStart = millisSinceEpoch();
+
     // Game loop
     while(glfwWindowShouldClose(window) == GL_FALSE) {
+
+        uint64_t millisNow = millisSinceEpoch();
+        uint32_t millisElapsed = millisNow - millisStart;
+
+        glm::vec4 colorThrob;
+        float throbAngle = 2 * PI_F * millisElapsed / THROB_PERIOD_MILLIS;
+        colorThrob = color * (0.5f * (1.0f + sinf(throbAngle)));
+        model->setColor(colorThrob);
 
         // Draw the scene
         model->drawRgb();
@@ -59,4 +77,9 @@ GLFWwindow* setupGl(void) {
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     return glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
 
+}
+
+uint64_t millisSinceEpoch()
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 }
