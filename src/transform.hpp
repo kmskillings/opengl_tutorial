55bcc30ea2684babb1f570773c7082f057d01c73
Kenneth Skillings <kmskillings@gmail.com>
2026-06-14 00:37:01 -0600

    Draft transform code

---
+++ src/transform.hpp
@@ -0,0 +1,36 @@
+#ifndef _TRANSFORM_HPP
+#define _TRANSFORM_HPP
+
+#include "gl_includes.h"
+
+namespace GlWorld
+{
+ 
+class Transform
+{
+public:
+    Transform(void);
+    const glm::mat4 getMatrixModel(void);
+    const glm::mat4 getMatrixModelInv(void);
+    const glm::vec3 getPosition(void);
+    void setPosition(const glm::vec3 &position);
+    void translate(const glm::vec3 &offset);
+    void translate(const glm::vec3 &direction, float distance);
+    const glm::quat getRotation(void);
+    void setRotation(const glm::quat &rotation);
+    void setRotation(float angle, const glm::vec3 axis);
+    void rotate(const glm::quat &rotation);
+    void rotate(float angle, const glm::vec3 axis);
+    const glm::vec3 getScale(void);
+    void setScale(const glm::vec3 &scale);
+    void scaleBy(const glm::vec3 &scale);
+private:
+    glm::vec3 position;
+    glm::quat rotation;
+    glm::vec3 scale;
+};
+
+}
+
+
+#endif
