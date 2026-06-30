56b6d4df4e0bea8f83d17ed1a443e171e287c6cf
Kenneth Skillings <kmskillings@gmail.com>
2026-06-29 22:11:37 -0600

    Fix const functions

--- src/transform.hpp
+++ src/transform.hpp
@@ -11,21 +11,37 @@ namespace GlWorld
 class Transform
 {
 public:
+
     Transform(void);
-    const glm::mat4 getMatrixModel(void);
-    const glm::mat4 getMatrixModelInv(void);
-    const glm::vec3 getPosition(void);
+
+    glm::mat4 getMatrixModel(void) const;
+
+    glm::mat4 getMatrixModelInv(void) const;
+
+    glm::vec3 getPosition(void) const;
+
     void setPosition(const glm::vec3 &position);
+
     void translate(const glm::vec3 &offset);
+
     void translate(const glm::vec3 &direction, float distance);
-    const glm::quat getRotation(void);
+
+    glm::quat getRotation(void) const;
+
     void setRotation(const glm::quat &rotation);
+
     void setRotation(float angle, const glm::vec3 axis);
+
     void rotate(const glm::quat &rotation);
+
     void rotate(float angle, const glm::vec3 axis);
-    const glm::vec3 getScale(void);
+
+    glm::vec3 getScale(void) const;
+
     void setScale(const glm::vec3 &scale);
+
     void scaleBy(const glm::vec3 &scale);
+
 private:
     glm::vec3 position;
     glm::quat rotation;
