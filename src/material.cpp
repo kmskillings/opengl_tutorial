158a4fa8993006ead33970f1f437005367923e10
Kenneth Skillings <kmskillings@gmail.com>
2026-07-01 22:36:51 -0600

    Tidy up 1x1 texture code

--- src/material.cpp
+++ src/material.cpp
@@ -242,8 +242,13 @@ GLuint create1x1Texture(
         type,
         data
     );
-    return texture;
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
     glBindTexture(GL_TEXTURE_2D, 0);
+    return texture;
 }
 
 }
