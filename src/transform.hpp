440df645d00cf2568f6afc9d1ab2c213f2d58448
Kenneth Skillings <kmskillings@gmail.com>
2026-07-06 16:07:59 -0600

    Draft new transform code

--- src/transform.hpp
+++ src/transform.hpp
@@ -7,6 +7,13 @@
 
 namespace GlWorld
 {
+
+enum class TransformAxes
+{
+    Local,
+    Parent,
+    Global
+};
  
 class Transform
 {
@@ -15,26 +22,34 @@ public:
     Transform(void);
 
     glm::mat4 getMatrixModel(void) const;
+    glm::mat4 getMatrixModel(TransformAxes axes) const;
 
     glm::mat4 getMatrixModelInv(void) const;
+    glm::mat4 getMatrixModelInv(TransformAxes axes) const;
 
     glm::vec3 getPosition(void) const;
+    glm::vec3 getPosition(TransformAxes axes) const;
 
     void setPosition(const glm::vec3 &position);
+    void setPosition(const glm::vec3 &position, TransformAxes axes);
 
     void translate(const glm::vec3 &offset);
-
+    void translate(const glm::vec3 &offset, TransformAxes axes);
     void translate(const glm::vec3 &direction, float distance);
+    void translate(const glm::vec3 &direction, float distance, TransformAxes axes);
 
     glm::quat getRotation(void) const;
+    glm::quat getRotation(TransformAxes axes) const;
 
     void setRotation(const glm::quat &rotation);
-
+    void setRotation(const glm::quat &rotation, TransformAxes axes);
     void setRotation(float angle, const glm::vec3 axis);
+    void setRotation(float angle, const glm::vec3 axis, TransformAxes axes);
 
     void rotate(const glm::quat &rotation);
-
+    void rotate(const glm::quat &rotation, TransformAxes axes);
     void rotate(float angle, const glm::vec3 axis);
+    void rotate(float angle, const glm::vec3 axis, TransformAxes axes);
 
     glm::vec3 getScale(void) const;
 
@@ -42,10 +57,14 @@ public:
 
     void scaleBy(const glm::vec3 &scale);
 
+    static const Transform& zero(void);
+
 private:
-    glm::vec3 position;
+    glm::vec3 position; // Stored in parent space.
     glm::quat rotation;
     glm::vec3 scale;
+    Transform* parent;
+    static Transform _zero;
 };
 
 class HasTransform
